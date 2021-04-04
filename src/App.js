144f1364e02b84afd09a649cc77ae88aec7a6d0d
Ashley Burns <ashley@ashleys-mini.home>
2021-04-04 00:08:36 -0400

    stuck but slowly getting there got caught up on wanting more than 6 people

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios'
+import Character from './components/Character'
+import Styled from 'styled-components'
+
 
 const App = () => {
+  const [people, setPeople] = useState([])
+  const [info, setInfo] = useState(null)
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  useEffect(() => {
+    Axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setPeople(res.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +28,16 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {people.map(obj => {
+        return (
+          <Character
+            key={obj.name}
+            starwars={obj}
+          />
+        )
+      })
+
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
-// Write your Character component here
+import React, { useEffect } from 'react'
+import Axios from 'axios'
+
+export default function Character(props) {
+    const { starwars } = props
+
+
+    return (
+        <div>
+            <h2>{starwars.name}</h2>
+
+
+            <button>See About</button>
+            <ul>
+                Gender: {starwars.gender}
+                Height: {starwars.height}
+                Mass: {starwars.mass}
+                Birth Year: {starwars.birth_year}
+                Eye Color: {starwars.eye_color}
+                Hair Color: {starwars.hair_color}
+                Skin Color: {starwars.skin_color}
+            </ul>
+        </div>
+    )
+}
